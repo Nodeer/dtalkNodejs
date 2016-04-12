07eff4e7232cccdd15a9483720565e65982d39d8
wongxming <mwongxming@gmail.com>
2016-04-12 16:56:43 +0800

    fix config

--- modules/dTalkVerifyUtil.js
+++ modules/dTalkVerifyUtil.js
@@ -3,67 +3,77 @@ var DTalkCrypt = require('./dTalkCrypt'),
     fs = require('fs'),
     path = require('path');
 
-var config = require("../data/dTalkConfig");
-console.log('dTalkConfig : ' + JSON.stringify(config));
-/*
-    "suite_ticket"事件每二十分钟推送一次,数据格式如下
-    {"SuiteKey": "suitexxxxxx","EventType": "suite_ticket","TimeStamp": 1234456,"SuiteTicket": "adsadsad"}
+var dTalkConfig = require("../data/dTalkConfig");
+console.log('dTalkConfig : ' + JSON.stringify(dTalkConfig));
+
+var config = {
+
+    token: dTalkConfig.token,
+    encodingAESKey: dTalkConfig.encodingAESKey,
+    suiteid: dTalkConfig.suiteid,
+    suitesecret: dTalkConfig.suitesecret,
+
+    /*
+        "suite_ticket"事件每二十分钟推送一次,数据格式如下
+        {"SuiteKey": "suitexxxxxx","EventType": "suite_ticket","TimeStamp": 1234456,"SuiteTicket": "adsadsad"}
+        */
+    getTicket: function(cb) {
+        //var self = this;
+        fs.readFile(path.resolve('./data/' + this.suiteid + '_ticket.json'), function(err, data) {
+            if (err) {
+                cb.error(err);
+
+            } else {
+                cb.success({ SuiteTicket: JSON.parse(data.toString()).SuiteTicket });
+            }
+
+        });
+    },
+    setTicket: function(data) {
+
+        fs.writeFile(path.resolve('./data/' + this.suiteid + '_ticket.json'), JSON.stringify(data));
+    },
+    /*
+    "tmp_auth_code"事件将企业对套件发起授权的时候推送,数据格式如下
+    {"SuiteKey": "suitexxxxxx", "EventType": " tmp_auth_code","TimeStamp": 1234456,"AuthCode": "adads"}            
     */
-config.prototype.getTicket = function(cb) {
-    //var self = this;
-    fs.readFile(path.resolve('./data/' + this.suiteid + '_ticket.json'), function(err, data) {
-        if (err) {
-            cb.error(err);
+    getToken: function(cb) {
 
-        } else {
-            cb.success({ SuiteTicket: JSON.parse(data.toString()).SuiteTicket });
-        }
+        fs.readFile(path.resolve('./data/' + this.suiteid + '_token.json'), function(err, data) {
+            if (err) {
+                cb.error(err);
 
-    });
-};
-config.prototype.setTicket = function(data) {
+            } else {
+                cb.success({ AuthCode: JSON.parse(data.toString()).AuthCode });
+            }
+        });
+    },
 
-    fs.writeFile(path.resolve('./data/' + this.suiteid + '_ticket.json'), JSON.stringify(data));
-};
-/*
-"tmp_auth_code"事件将企业对套件发起授权的时候推送,数据格式如下
-{"SuiteKey": "suitexxxxxx", "EventType": " tmp_auth_code","TimeStamp": 1234456,"AuthCode": "adads"}            
-*/
-config.prototype.getToken = function(cb) {
+    setToken: function(data) {
 
-    fs.readFile(path.resolve('./data/' + this.suiteid + '_token.json'), function(err, data) {
-        if (err) {
-            cb.error(err);
+        fs.writeFile(path.resolve('./data/' + this.suiteid + '_token.json'), JSON.stringify(data));
+    },
+    //{"permanent_code": "xxxx","auth_corp_info":{"corpid": "xxxx","corp_name": "name"}}
+    getPermanentCode: function(corpId, cb) {
 
-        } else {
-            cb.success({ AuthCode: JSON.parse(data.toString()).AuthCode });
-        }
-    });
-};
+        fs.readFile(path.resolve('./data/' + this.suiteid + '_' + corpId + '_permanent_code.json'), function(err, data) {
 
-config.prototype.setToken = function(data) {
+            if (err) {
+                cb.error(err);
 
-    fs.writeFile(path.resolve('./data/' + this.suiteid + '_token.json'), JSON.stringify(data));
-};
-//{"permanent_code": "xxxx","auth_corp_info":{"corpid": "xxxx","corp_name": "name"}}
-config.prototype.getPermanentCode = function(corpId, cb) {
+            } else {
+                cb.success({ permanentCode: JSON.parse(data.toString()).permanent_code });
+            }
+        });
+    },
 
-    fs.readFile(path.resolve('./data/' + this.suiteid + '_' + corpId + '_permanent_code.json'), function(err, data) {
+    setPermanentCode: function(corpInfo) {
 
-        if (err) {
-            cb.error(err);
+        fs.writeFile(path.resolve('./data/' + this.suiteid + '_' + corpInfo.auth_corp_info.corpid + '_permanent_code.json'), JSON.stringify(corpInfo));
+    }
 
-        } else {
-            cb.success({ permanentCode: JSON.parse(data.toString()).permanent_code });
-        }
-    });
-};
 
-config.prototype.setPermanentCode = function(corpInfo) {
-
-    fs.writeFile(path.resolve('./data/' + this.suiteid + '_' + corpInfo.auth_corp_info.corpid + '_permanent_code.json'), JSON.stringify(corpInfo));
-};
-
+}
 var dTalkCrypt = new DTalkCrypt(config.token, config.encodingAESKey, config.suiteid || 'suite4xxxxxxxxxxxxxxx');
 
 var nonce_success = 'success';
