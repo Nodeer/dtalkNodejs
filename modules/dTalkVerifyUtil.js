d41d350bd3d746518e955dadbb0ae51478aca7d6
wongxming <wongxming@qq.com>
2016-04-12 16:47:31 +0800

    fix config.prototype

--- modules/dTalkVerifyUtil.js
+++ modules/dTalkVerifyUtil.js
@@ -4,68 +4,64 @@ var DTalkCrypt = require('./dTalkCrypt'),
     path = require('path');
 
 var config = require("../data/dTalkConfig");
-console.log('dTalkConfig : '+JSON.stringify(config));
-config.prototype = {
-    /*
+console.log('dTalkConfig : ' + JSON.stringify(config));
+/*
     "suite_ticket"事件每二十分钟推送一次,数据格式如下
     {"SuiteKey": "suitexxxxxx","EventType": "suite_ticket","TimeStamp": 1234456,"SuiteTicket": "adsadsad"}
     */
+config.prototype.getTicket = function(cb) {
+    //var self = this;
+    fs.readFile(path.resolve('./data/' + this.suiteid + '_ticket.json'), function(err, data) {
+        if (err) {
+            cb.error(err);
 
-    getTicket: function(cb) {
-        //var self = this;
-        fs.readFile(path.resolve('./data/' + this.suiteid + '_ticket.json'), function(err, data) {
-            if (err) {
-                cb.error(err);
+        } else {
+            cb.success({ SuiteTicket: JSON.parse(data.toString()).SuiteTicket });
+        }
 
-            } else {
-                cb.success({ SuiteTicket: JSON.parse(data.toString()).SuiteTicket });
-            }
+    });
+};
+config.prototype.setTicket = function(data) {
 
-        });
-    },
-    setTicket: function(data) {
+    fs.writeFile(path.resolve('./data/' + this.suiteid + '_ticket.json'), JSON.stringify(data));
+};
+/*
+"tmp_auth_code"事件将企业对套件发起授权的时候推送,数据格式如下
+{"SuiteKey": "suitexxxxxx", "EventType": " tmp_auth_code","TimeStamp": 1234456,"AuthCode": "adads"}            
+*/
+config.prototype.getToken = function(cb) {
 
-        fs.writeFile(path.resolve('./data/' + this.suiteid + '_ticket.json'), JSON.stringify(data));
-    },
-    /*
-    "tmp_auth_code"事件将企业对套件发起授权的时候推送,数据格式如下
-    {"SuiteKey": "suitexxxxxx", "EventType": " tmp_auth_code","TimeStamp": 1234456,"AuthCode": "adads"}            
-    */
-    getToken: function(cb) {
+    fs.readFile(path.resolve('./data/' + this.suiteid + '_token.json'), function(err, data) {
+        if (err) {
+            cb.error(err);
 
-        fs.readFile(path.resolve('./data/' + this.suiteid + '_token.json'), function(err, data) {
-            if (err) {
-                cb.error(err);
+        } else {
+            cb.success({ AuthCode: JSON.parse(data.toString()).AuthCode });
+        }
+    });
+};
 
-            } else {
-                cb.success({ AuthCode: JSON.parse(data.toString()).AuthCode });
-            }
-        });
-    },
+config.prototype.setToken = function(data) {
 
-    setToken: function(data) {
+    fs.writeFile(path.resolve('./data/' + this.suiteid + '_token.json'), JSON.stringify(data));
+};
+//{"permanent_code": "xxxx","auth_corp_info":{"corpid": "xxxx","corp_name": "name"}}
+config.prototype.getPermanentCode = function(corpId, cb) {
 
-        fs.writeFile(path.resolve('./data/' + this.suiteid + '_token.json'), JSON.stringify(data));
-    },
-    //{"permanent_code": "xxxx","auth_corp_info":{"corpid": "xxxx","corp_name": "name"}}
-    getPermanentCode: function(corpId, cb) {
+    fs.readFile(path.resolve('./data/' + this.suiteid + '_' + corpId + '_permanent_code.json'), function(err, data) {
 
-        fs.readFile(path.resolve('./data/' + this.suiteid + '_' + corpId + '_permanent_code.json'), function(err, data) {
+        if (err) {
+            cb.error(err);
 
-            if (err) {
-                cb.error(err);
+        } else {
+            cb.success({ permanentCode: JSON.parse(data.toString()).permanent_code });
+        }
+    });
+};
 
-            } else {
-                cb.success({ permanentCode: JSON.parse(data.toString()).permanent_code });
-            }
-        });
-    },
+config.prototype.setPermanentCode = function(corpInfo) {
 
-    setPermanentCode: function(corpInfo) {
-
-        fs.writeFile(path.resolve('./data/' + this.suiteid + '_' + corpInfo.auth_corp_info.corpid + '_permanent_code.json'), JSON.stringify(corpInfo));
-    }
-
+    fs.writeFile(path.resolve('./data/' + this.suiteid + '_' + corpInfo.auth_corp_info.corpid + '_permanent_code.json'), JSON.stringify(corpInfo));
 };
 
 var dTalkCrypt = new DTalkCrypt(config.token, config.encodingAESKey, config.suiteid || 'suite4xxxxxxxxxxxxxxx');
